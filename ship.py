d860af0b40e176d240436de3a5a5b33460f3219f
codexknight <dfotache@gmail.com>
2020-04-04 20:41:41 +0300

    adjusted the ship's speed

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
 
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/ship.bmp')
@@ -12,15 +13,19 @@ class Ship:
 
         self.image_rect.midbottom = self.screen_rect.midbottom
 
+        self.x = float(self.image_rect.x)
+
         self.moving_right = False
-        self.moving_left=False
+        self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.image_rect.x += 1
+            self.x += self.settings.ship_speed
 
         if self.moving_left:
-            self.image_rect.x -=1
+            self.x -= self.settings.ship_speed
+
+        self.image_rect.x=self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.image_rect)
